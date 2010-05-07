1b1c78fd6f4992ce9dc951cbe126693bcce607bb
Matt <matt.beedle@1000jobboersen.de>
2010-05-07 19:22:32 +0200

    Removed all the xml crap, which for some weird reason, was blowing up my site

--- lib/controllers/translations_controller.rb
+++ lib/controllers/translations_controller.rb
@@ -5,19 +5,10 @@ class TranslationsController < ActionController::Base
 
   ## FIXME:  you'll probably want add authorization to this controller!
 
-  # GET /translations
-  # GET /translations.xml
   def index
     @translations = @locale.translations.all(:order => "raw_key, pluralization_index")
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.xml  { render :xml => @translations }
-    end
   end
 
-  # GET /translations
-  # GET /translations.xml
   def translations
     @locale ||= Locale.default_locale
     @translation_option = TranslationOption.find(params[:translation_option])
@@ -27,15 +18,8 @@ class TranslationsController < ActionController::Base
     else
       @translations = @locale.translations.untranslated
     end
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.xml  { render :xml => @translations }
-    end
   end
 
-  # GET /asset_translations
-  # GET /asset_translations.xml
   def asset_translations
     @locale ||= Locale.default_locale
     @translation_option = TranslationOption.find(params[:translation_option])
@@ -49,75 +33,40 @@ class TranslationsController < ActionController::Base
     else
       @asset_translations = @untranslated_assets
     end
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.xml  { render :xml => @untranslated_assets }
-    end
   end
 
-  # GET /translations/1
-  # GET /translations/1.xml
   def show
     @translation = @locale.translations.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.xml  { render :xml => @translation }
-    end
   end
 
-  # GET /translations/new
-  # GET /translations/new.xml
   def new
     @translation = Translation.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.xml  { render :xml => @translation }
-    end
   end
 
-  # GET /translations/1/edit
   def edit
     @translation = @locale.translations.find(params[:id])
   end
 
-  # POST /translations
-  # POST /translations.xml
   def create
     @translation = @locale.translations.build(params[:translation])
 
-    respond_to do |format|
-      if @translation.save
-        flash[:notice] = 'Translation was successfully created.'
-        format.html { redirect_to locale_translation_path(@locale, @translation) }
-        format.xml  { render :xml => @translation, :status => :created, :location => @translation }
-      else
-        format.html { render :action => "new" }
-        format.xml  { render :xml => @translation.errors, :status => :unprocessable_entity }
-      end
+    if @translation.save
+      flash[:notice] = 'Translation was successfully created.'
+      redirect_to locale_translation_path(@locale, @translation)
+    else
+      render :action => "new"
     end
   end
 
-  # PUT /translations/1
-  # PUT /translations/1.xml
   def update
     @translation  = @locale.translations.find(params[:id])
     @first_time_translating = @translation.value.nil?
 
-    respond_to do |format|
-      if @translation.update_attributes(params[:translation])
-        format.html do
-          flash[:notice] = 'Translation was successfully updated.'
-          redirect_to locale_translation_path(@locale, @translation)
-        end
-        format.xml  { head :ok }
-        format.js   {}
-      else
-        format.html { render :action => "edit" }
-        format.xml  { render :xml => @translation.errors, :status => :unprocessable_entity }
-      end
+    if @translation.update_attributes(params[:translation])
+      flash[:notice] = 'Translation was successfully updated.'
+      redirect_to locale_translation_path(@locale, @translation)
+    else
+      render :action => "edit"
     end
   end
 
@@ -126,11 +75,7 @@ class TranslationsController < ActionController::Base
   def destroy
     @translation = @locale.translations.find(params[:id])
     @translation.destroy
-
-    respond_to do |format|
-      format.html { redirect_to(locale_translations_url) }
-      format.xml  { head :ok }
-    end
+    redirect_to(locale_translations_url)
   end
 
   private
